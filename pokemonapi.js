eabfeb39d7698372ac2e4bc409145bfb1ed00aba
Aiden Hu <125307388+TheCoder122@users.noreply.github.com>
2026-01-15 20:00:29 -0500

    Add files via upload

---
+++ pokemonapi.js
@@ -0,0 +1,14 @@
+async function getData(poke) {
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/${poke}/');
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      console.log(data);
+      document.getElementById("api-response").textContent = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+getData("pikachu");
